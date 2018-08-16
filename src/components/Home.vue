028e76002da133c2a23e50de95ccb6786b2b1cc9
Sam Lesem <samuel.lesem@gmail.com>
2018-08-15 23:48:04 -0500

    hr and new html classes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,25 @@
 <template>
   <b-container fluid id="bg1">
-    <b-row>
-      <header>
-        <h1>Sam Lesem</h1>
-        <h3>Software Engineer, UnitedHealth Group</h3>
-      </header>
+
+    <b-row class="row-1">
+      <b-col class="row-1-col-1">
+        <hr>
+        <header>
+          <h1>Sam Lesem</h1>
+          <h3>Software Engineer, Data Consumer, Netflix Enthusiest</h3>
+        </header>
+      </b-col>
     </b-row>
-    <b-row align-v="center" class="row-2">
+    <b-row class="row-2">
       <b-col>
         <b-img src="./src/assets/IMG_6193.png" center rounded="circle" width="576px" height="384px" class="m-1" />
       </b-col>
-
       <b-col>
       </b-col>
     </b-row>
+    <hr>
     <footer>
-      GitHub
+
     </footer>
 
   </b-container>
@@ -26,7 +30,7 @@ export default {};
 </script>
 
 <style scoped>
-header {
+.row-1-col-1 {
   float: left;
   padding: 2em 4em;
   height: 500px;
@@ -35,16 +39,20 @@ header {
   color: white;
 }
 
+hr {
+  border: 0;
+  height: 1px;
+  background: #333;
+  background-image: linear-gradient(to right, #ccc, #333, #ccc);
+}
+
 .row-2 {
   margin-top: 100px;
+  margin-bottom: 100px;
 }
 
 #bg1 {
   font-family: "Open Sans", sans-serif;
-}
-
-footer {
-  float: left;
 }
 </style>
 
