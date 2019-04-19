66e4cf845bf3865659a4df23d420132e5a9b5701
Sam Lesem <samuel.lesem@gmail.com>
2019-04-19 10:24:13 -0500

    [SL] 4/19: webpack client key

--- webpack.config.js
+++ webpack.config.js
@@ -82,7 +82,8 @@ if (process.env.NODE_ENV === 'production') {
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: '"production"'
+        NODE_ENV: '"production"',
+        MAPBOX_KEY: JSON.stringify(process.env.MAPBOX_KEY)
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
